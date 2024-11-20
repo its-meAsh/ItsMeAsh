4efc1eab3e32bb552997531b2d2c4d63826d887d
Ash <136570879+its-meAsh@users.noreply.github.com>
2024-11-20 22:30:35 +0530

    Update common.js

--- common.js
+++ common.js
@@ -86,16 +86,7 @@ function matrixMake(r,c,v){
   return array
 }
 
-function randColor(){
-  let hexArray = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
-  let color = "#";
-  for (let i = 0; i<6;i++){
-    color+=choice(hexArray);
-  }
-  return color
-}
-
-      function getFreq(array){
+function getFreq(array){
   let note = {}
   for (let each of array){
     if (!checkIn(each,Object.keys(note))){
@@ -104,4 +95,52 @@ function randColor(){
     note[each]++;
   }
   return note
+}
+
+
+function decToHex(num){
+  let decToHexMap = {
+    0:"0",1:"1",2:"2",3:"3",4:"4",5:"5",6:"6",7:"7",8:"8",9:"9",10:"A",11:"B",12:"C",13:"D",14:"E",15:"F"
+  };
+  let hex = ""
+  while ( num > 0){
+    let r = num%16;
+    hex=(decToHexMap[r]+hex);
+    num = Math.floor(num/16);
+  }
+  return hex
+}
+
+function fillZero(text,length){
+  let len = text.length
+  for (let i = 0; i<length-len;i++){
+    text = "0"+text;
+    console.log(text,i)
+  }
+  return text
+}
+
+function randHex(length){
+  let hexArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+  let hex = "";
+  for (let i = 0; i < length; i++) {
+    hex += choice(hexArray);
+  }
+  return hex
+}
+
+function randColor() {
+  let color = `#${randHex(6)}`
+  return color
+}
+
+function hexToDec(hex){
+  let hexToDecMap = {
+    "0":0,"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"A":10,"B":11,"C":12,"D":13,"E":14,"F":15
+  };
+  let sum = 0;
+  for (let i = 0; i<hex.length;i++){
+    sum+=(16**(hex.length-1-i))*(hexToDecMap[hex[i]])
+  }
+  return sum
     }
